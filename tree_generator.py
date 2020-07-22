ba9f74f4f206e7eea71887a0257fafc1b0e3efaa
Logan <loganmichellemurphy@gmail.com>
2020-07-22 16:49:44 -0500

    Added function 'add_value' that creates string sequence for the values under branches.

--- tree_generator.py
+++ tree_generator.py
@@ -3,18 +3,18 @@ def space(num):
 
 def branch(amount, spacing=4):
     return("|" + ((space(spacing) + "\\") * (amount-1)))
-    
 
-empty_string = ""
+def add_values(value_list, spacing=4):
+    placeholder_string = ""
+    for i in range(branch_amount):
+        placeholder_string += str(value_list[i]) + space(spacing)
+    return placeholder_string
 
 top = 1
 
 branch_amount = 3
 
 value_list = [2,3,4]
-
-for i in range(branch_amount):
-    empty_string = empty_string + str(value_list[i]) + space(4)
 
 print(top)
 
@@ -23,4 +23,4 @@ if branch_amount == 1:
 elif branch_amount > 1:
     print(branch(branch_amount))
 
-print(empty_string)
+print(add_values(value_list))
