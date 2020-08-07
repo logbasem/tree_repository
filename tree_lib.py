ff07f83b7a3cdd9396ca24cbc9197d637c6873ca
Logan <loganmichellemurphy@gmail.com>
2020-08-07 17:02:55 -0500

    Created a prime number tree for 150

--- tree_lib.py
+++ tree_lib.py
@@ -4,8 +4,12 @@ from treelib import Node, Tree
 
 tree = Tree()
 
-tree.create_node("1",1)
-tree.create_node("2",2,parent=1)
-tree.create_node("3",3,parent=1)
+tree.create_node("150",150)
+tree.create_node("15",15,parent=150)
+tree.create_node("10",10,parent=150)
+tree.create_node("5",5,parent=15)
+tree.create_node("3",3,parent=15)
+tree.create_node("5",5.2,parent=10)
+tree.create_node("2",2,parent=10)
 
 tree.show()
