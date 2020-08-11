3c8f3ff6d371784cbd7a52668e6a2cf274c28f95
Logan <loganmichellemurphy@gmail.com>
2020-08-11 17:05:02 -0500

    Created a function to add two branches to a parent, began creating a 'create_tree' function.

---
+++ binary_tree_generator.py
@@ -0,0 +1,23 @@
+import treelib
+
+from treelib import Node, Tree
+
+
+
+def add_two_branches(tree,parent,first_value,second_value):
+    tree.create_node(str(first_value),first_value,parent=parent)
+    tree.create_node(str(second_value),second_value,parent=parent)
+
+def create_tree(tier_number,top,value_list):
+    binary_tree = Tree()
+    binary_tree.create_node(str(top),top)
+
+    add_two_branches(binary_tree,top,value_list[0],value_list[1])
+
+    binary_tree.show()
+
+tier_number = 2
+top = 150
+value_list = [15,10]
+
+create_tree(tier_number,top,value_list)
